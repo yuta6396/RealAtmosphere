33449dcb1d08cfb0c5c5d126cab6692d17ea06e2
Yuta_WS <higuchi141226@icloud.com>
2024-12-13 18:47:37 +0900

    1213

--- sim_BO_var4.py
+++ sim_BO_var4.py
@@ -56,15 +56,15 @@ file_path = '/home/yuta/scale-5.5.3/scale-rm/test/tutorial/real/experiment_init_
 gpyoptfile=f"BO_init_4var.pe######.nc"
 
 # 関数評価回数
-update_batch_times = 1
-batch_size=5
-opt_num=batch_size*update_batch_times  #WSでは<=24
+update_batch_times = 20
+batch_size=5 #WSでは<=6
+opt_num=batch_size*update_batch_times  
 
-trial_num = 1 # 試行回数
+trial_num = 7 # 試行回数
 
 # 降水強度を最小化したい領域
-Objective_X_low = 70
-Objective_X_high = 80
+Objective_X_low = 65
+Objective_X_high = 75
 Objective_Y_low = 60
 Objective_Y_high = 70
 Area_size=(Objective_X_high-Objective_X_low)*(Objective_Y_high-Objective_Y_low)
@@ -72,23 +72,23 @@ Area_size=(Objective_X_high-Objective_X_low)*(Objective_Y_high-Objective_Y_low)
 Objective_T = 5
 
 # 制御する変数
-Control_Var = "MOMY"
+Control_Var_name = "RHOT"
 
 # 制御対象範囲
-Control_X_low = 0
+Control_X_low = 45
 Control_X_high = 90 #MAX =90
 Control_Y_low = 0
-Control_Y_high = 90 #MAX =90
+Control_Y_high = 45 #MAX =90
 Control_Z_low = 0
-Control_Z_high = 1 #MAX =35?
+Control_Z_high = 35 #MAX =35?
 
 # 介入領域の大きさ
 Control_X_size = 5 #あんま変えられない　いくつか同時に変更する地点ありrandom_samples1 とか
 Control_Y_size = 5
 
 #　介入幅
-Control_MOMY_low = -20 # -30~30でエラー確認済み
-Control_MOMY_high = 20
+Control_Var_low = 0 # -30~30でエラー確認済み
+Control_Var_high = 10
 
 
 
@@ -97,7 +97,7 @@ Control_MOMY_high = 20
 Base_estimator="GP"
 Acq_func="EI"
 
-base_dir = f"../test_result/PREC_t=5_{Control_X_size}*{Control_Y_size}grids_FET={opt_num}_trials={trial_num}_{current_time}"
+base_dir = f"../test_result/{Control_Var_name}_t={Objective_T}_{Control_X_size}*{Control_Y_size}grids_FET={opt_num}_trials={trial_num}_{current_time}"
 
 
 def predict(inputs):
@@ -161,19 +161,19 @@ def predict(inputs):
     f.write(f"t={Objective_T}:Ave of PREC in X[{Objective_X_low}, {Objective_X_high-1}], Y[{Objective_Y_low}, {Objective_Y_high-1}] = {result} [mm/h]")
     return result
 
-def init_val_intervation(num,Center_Control_X,Center_Control_Y,Control_Z,Control_MOMY):
+def init_val_intervation(num,Center_Control_X,Center_Control_Y,Control_Z,Control_Var):
     """
     与えられた制御変数の値（Center_Control_X~4）を用いて初期値を変更する。
     """
     
     global org_file
-    f.write(f"control Center_Control_X={Center_Control_X},Center_Control_Y={Center_Control_Y},Control_Z={Control_Z},Control_MOMY={Control_MOMY}\n")
-    print(f"control Center_Control_X={Center_Control_X},Center_Control_Y={Center_Control_Y},Control_Z={Control_Z},Control_MOMY={Control_MOMY}\n")
+    f.write(f"control Center_Control_X={Center_Control_X},Center_Control_Y={Center_Control_Y},Control_Z={Control_Z},Control_Var={Control_Var}\n")
+    print(f"control Center_Control_X={Center_Control_X},Center_Control_Y={Center_Control_Y},Control_Z={Control_Z},Control_Var={Control_Var}\n")
     for pe in range(nofpe):
-        output_file = f"000{num}/out-{Control_Var}.pe######.nc"
+        output_file = f"000{num}/out-{Control_Var_name}.pe######.nc"
         sub_init_file = f"000{num}/init_d01_20070714-180000.000.pe######.nc"
         if num>=10:
-            output_file = f"00{num}/out-{Control_Var}.pe######.nc"
+            output_file = f"00{num}/out-{Control_Var_name}.pe######.nc"
             sub_init_file = f"00{num}/init_d01_20070714-180000.000.pe######.nc"
         sub_init = sub_init_file.replace('######', str(pe).zfill(6))
         output = output_file.replace('######', str(pe).zfill(6))
@@ -187,28 +187,28 @@ def init_val_intervation(num,Center_Control_X,Center_Control_Y,Control_Z,Control
                 x = dst.createVariable(
                     name, variable.datatype, variable.dimensions)
                 dst[name].setncatts(src[name].__dict__)
-                if name == Control_Var:
+                if name == Control_Var_name:
                     var = src[name][:]
                     print(var.shape) # 出力　(47, 47, 36)
                     if pe == 0:
                         if Center_Control_X<X_size/fnx and Center_Control_Y<X_size/fnx:
-                            var[int(Center_Control_Y)-2:int(Center_Control_Y)+3,int(Center_Control_X)-2:int(Center_Control_X)+3 , int(Control_Z)] += Control_MOMY # (y, x, z)
+                            var[int(Center_Control_Y)-2:int(Center_Control_Y)+3,int(Center_Control_X)-2:int(Center_Control_X)+3 , int(Control_Z)] += Control_Var # (y, x, z) 3:5なら3, 4のみ
                     elif pe == 1:
                         if Center_Control_X>=X_size/fnx and Center_Control_Y<X_size/fnx:
-                            var[int(Center_Control_Y)-2:int(Center_Control_Y)+3, int(Center_Control_X-X_size/fnx)-2:int(Center_Control_X-X_size/fnx)+3, int(Control_Z)] += Control_MOMY
+                            var[int(Center_Control_Y)-2:int(Center_Control_Y)+3, int(Center_Control_X-X_size/fnx)-2:int(Center_Control_X-X_size/fnx)+3, int(Control_Z)] += Control_Var
                     elif pe==2:
                         if Center_Control_X<X_size/fnx and Center_Control_Y>=X_size/fnx:
-                            var[int(Center_Control_Y-X_size/fnx)-2:int(Center_Control_Y-X_size/fnx)+3, int(Center_Control_X)-2:int(Center_Control_X)+3, int(Control_Z)] += Control_MOMY
+                            var[int(Center_Control_Y-X_size/fnx)-2:int(Center_Control_Y-X_size/fnx)+3, int(Center_Control_X)-2:int(Center_Control_X)+3, int(Control_Z)] += Control_Var
                     elif pe==3:
                         if Center_Control_X>=X_size/fnx and Center_Control_Y>=X_size/fnx:
-                            var[int(Center_Control_Y-X_size/fnx)-2:int(Center_Control_Y-X_size/fnx)+3, int(Center_Control_X-X_size/fnx)-2:int(Center_Control_X-X_size/fnx)+3, int(Control_Z)] += Control_MOMY
+                            var[int(Center_Control_Y-X_size/fnx)-2:int(Center_Control_Y-X_size/fnx)+3, int(Center_Control_X-X_size/fnx)-2:int(Center_Control_X-X_size/fnx)+3, int(Control_Z)] += Control_Var
                     dst[name][:] = var
                 else:
                     dst[name][:] = src[name][:]
         subprocess.run(["cp", output, sub_init ])
     return
 
-def sim(Center_Control_X,Center_Control_Y,Control_Z,Control_MOMY):
+def sim(Center_Control_X,Center_Control_Y,Control_Z,Control_Var):
     """
     得られた最適解を用いて目的関数の値を再度計算する。
     制御しない場合と制御した場合における、ある時刻のある領域の降水強度の値を返す。
@@ -222,7 +222,7 @@ def sim(Center_Control_X,Center_Control_Y,Control_Z,Control_MOMY):
     CTRL_prec_sum=0
 
     for pe in range(nofpe):
-        output_file = f"out-{Control_Var}.pe######.nc"
+        output_file = f"out-{Control_Var_name}.pe######.nc"
         # input file
         init = init_file.replace('######', str(pe).zfill(6))
         org = org_file.replace('######', str(pe).zfill(6))
@@ -246,20 +246,20 @@ def sim(Center_Control_X,Center_Control_Y,Control_Z,Control_MOMY):
                     name, variable.datatype, variable.dimensions)
                 # copy variable attributes all at once via dictionary
                 dst[name].setncatts(src[name].__dict__)
-                if name == Control_Var:
+                if name == Control_Var_name:
                     var = src[name][:]
                     if pe == 0:
                         if Center_Control_X<X_size/fnx and Center_Control_Y<X_size/fnx:
-                            var[int(Center_Control_Y)-2:int(Center_Control_Y)+3,int(Center_Control_X)-2:int(Center_Control_X)+3 , int(Control_Z)] += Control_MOMY
+                            var[int(Center_Control_Y)-2:int(Center_Control_Y)+3,int(Center_Control_X)-2:int(Center_Control_X)+3 , int(Control_Z)] += Control_Var
                     elif pe == 1:
                         if Center_Control_X>=X_size/fnx and Center_Control_Y<X_size/fnx:
-                            var[int(Center_Control_Y)-2:int(Center_Control_Y)+3, int(Center_Control_X-X_size/fnx)-2:int(Center_Control_X-X_size/fnx)+3, int(Control_Z)] += Control_MOMY
+                            var[int(Center_Control_Y)-2:int(Center_Control_Y)+3, int(Center_Control_X-X_size/fnx)-2:int(Center_Control_X-X_size/fnx)+3, int(Control_Z)] += Control_Var
                     elif pe==2:
                         if Center_Control_X<X_size/fnx and Center_Control_Y>=X_size/fnx:
-                            var[int(Center_Control_Y-X_size/fnx)-2:int(Center_Control_Y-X_size/fnx)+3, int(Center_Control_X)-2:int(Center_Control_X)+3, int(Control_Z)] += Control_MOMY
+                            var[int(Center_Control_Y-X_size/fnx)-2:int(Center_Control_Y-X_size/fnx)+3, int(Center_Control_X)-2:int(Center_Control_X)+3, int(Control_Z)] += Control_Var
                     elif pe==3:
                         if Center_Control_X>=X_size/fnx and Center_Control_Y>=X_size/fnx:
-                            var[int(Center_Control_Y-X_size/fnx)-2:int(Center_Control_Y-X_size/fnx)+3, int(Center_Control_X-X_size/fnx)-2:int(Center_Control_X-X_size/fnx)+3, int(Control_Z)] += Control_MOMY
+                            var[int(Center_Control_Y-X_size/fnx)-2:int(Center_Control_Y-X_size/fnx)+3, int(Center_Control_X-X_size/fnx)-2:int(Center_Control_X-X_size/fnx)+3, int(Control_Z)] += Control_Var
                   
                     dst[name][:] = var
                 else:
@@ -339,34 +339,35 @@ def plot_PREC(trial_i, TEST_CTRL_prec_matrix):
 
     # unit conversion (kg/m/s --> mm/d)
     var *=  60 * 60
+    print(f"{len(var)=}")
+    for t in range(len(var)):
+        # plot
+        fig = plt.figure()
 
-    # plot
-    fig = plt.figure()
+        # scale
+        ax = fig.add_subplot(1,1,1,projection=ccrs.PlateCarree())
+        ax.set_extent(plt_extent)
+        ax.coastlines(resolution='50m',linewidth=0.5)
 
-    # scale
-    ax = fig.add_subplot(1,1,1,projection=ccrs.PlateCarree())
-    ax.set_extent(plt_extent)
-    ax.coastlines(resolution='50m',linewidth=0.5)
+        # grid line
+        gl = ax.gridlines(crs=ccrs.PlateCarree(),draw_labels=True,linewidth=0.5,linestyle='--',color='gray')
+        gl.xlocator = mticker.FixedLocator(range(126,146))
+        gl.ylocator = mticker.FixedLocator(range(26,44))
+        gl.top_labels = False
+        gl.right_labels = False
+        gl.xlabel_style = {'size':6}
+        gl.ylabel_style = {'size':6}
 
-    # grid line
-    gl = ax.gridlines(crs=ccrs.PlateCarree(),draw_labels=True,linewidth=0.5,linestyle='--',color='gray')
-    gl.xlocator = mticker.FixedLocator(range(126,146))
-    gl.ylocator = mticker.FixedLocator(range(26,44))
-    gl.top_labels = False
-    gl.right_labels = False
-    gl.xlabel_style = {'size':6}
-    gl.ylabel_style = {'size':6}
+        im = ax.contourf(slon,slat,var[t],cmap=cmap,levels=m_levels, extend='both',transform=ccrs.PlateCarree())
 
-    im = ax.contourf(slon,slat,var[5],cmap=cmap,levels=m_levels, extend='both',transform=ccrs.PlateCarree())
+        pos = ax.get_position()
+        fig.subplots_adjust(right=0.85)
+        cax = fig.add_axes([pos.x1,pos.y0,0.02,pos.y1-pos.y0])
+        clb = fig.colorbar(im, orientation='vertical', cax=cax)
 
-    pos = ax.get_position()
-    fig.subplots_adjust(right=0.85)
-    cax = fig.add_axes([pos.x1,pos.y0,0.02,pos.y1-pos.y0])
-    clb = fig.colorbar(im, orientation='vertical', cax=cax)
-
-    filename = os.path.join(base_dir, "PREC-heatmap", f"LontLat_seed={trial_i}.png")
-    plt.savefig(filename ,dpi=300)
-    plt.close()
+        filename = os.path.join(base_dir, "PREC-heatmap", f"LonLat_t={t}_seed={trial_i}.png")
+        plt.savefig(filename ,dpi=300)
+        plt.close()
 
     # ヒートマップの作成 緯度経度でなくグリッド
     ## 制御後の降水強度
@@ -374,7 +375,7 @@ def plot_PREC(trial_i, TEST_CTRL_prec_matrix):
     plt.grid(True)
     plt.imshow(TEST_prec_matrix.T, cmap='viridis', aspect='auto',origin='lower')  # カラーマップは好みに応じて変更可能
     plt.colorbar(label="precipitation (mm/h)")  # カラーバーのラベル
-    plt.title("precipitation")
+    plt.title(f"Time={Objective_T}h_X={Objective_X_low}-{Objective_X_high-1}_Y={Objective_Y_low}-{Objective_Y_high-1} ")
     plt.xlabel("X")
     plt.ylabel("Y")
     filename = os.path.join(base_dir, "PREC-heatmap", f"Grid_seed={trial_i}.png")
@@ -385,7 +386,7 @@ def plot_PREC(trial_i, TEST_CTRL_prec_matrix):
     plt.grid(True)
     plt.imshow(TEST_CTRL_prec_matrix.T, cmap='viridis', aspect='auto',origin='lower')  # カラーマップは好みに応じて変更可能
     plt.colorbar(label="precipitation (mm/h)")  # カラーバーのラベル
-    plt.title("precipitation")
+    plt.title(f"Time={Objective_T}h_X={Objective_X_low}-{Objective_X_high-1}_Y={Objective_Y_low}-{Objective_Y_high-1} ")
     plt.xlabel("X")
     plt.ylabel("Y")
     filename = os.path.join(base_dir, "PREC-heatmap", f"Grid_diff_C-noC_seed={trial_i}.png")
@@ -431,7 +432,7 @@ with open(config_file_path,  "w") as f:
     
     # 制御する変数
     f.write("\n# 制御する変数\n")
-    f.write(f"Control_Var = \"{Control_Var}\"\n")
+    f.write(f"Control_Var_name= \"{Control_Var_name}\"\n")
     
     # 制御対象範囲
     f.write("\n# 制御対象範囲\n")
@@ -449,8 +450,8 @@ with open(config_file_path,  "w") as f:
     
     # 介入幅
     f.write("\n# 介入幅\n")
-    f.write(f"Control_MOMY_low = {Control_MOMY_low}  # -30~30でエラー確認済み\n")
-    f.write(f"Control_MOMY_high = {Control_MOMY_high}\n")
+    f.write(f"Control_Var_low = {Control_Var_low}  # -30~30でエラー確認済み\n")
+    f.write(f"Control_Var_high = {Control_Var_high}\n")
     
     # BOの獲得関数
     f.write("\n# BOの獲得関数\n")
@@ -460,7 +461,7 @@ with open(config_file_path,  "w") as f:
 log_file = os.path.join(base_dir, "summary", f"BO_log.txt")
 
 # 入力次元と最小値・最大値の定義
-bounds = [ Integer(Control_X_low+2, Control_X_high-3),Integer(Control_Y_low+2, Control_Y_high-3) ,Integer(Control_Z_low, Control_Z_high),Real(Control_MOMY_low,Control_MOMY_high)]
+bounds = [ Integer(Control_X_low+2, Control_X_high-3),Integer(Control_Y_low+2, Control_Y_high-3) ,Integer(Control_Z_low, Control_Z_high),Real(Control_Var_low,Control_Var_high)] # Int(2, 4) なら2, 3, 4からランダム
 
 with open(log_file, 'w') as f:
     for trial_i in range(trial_num):
@@ -482,7 +483,7 @@ with open(log_file, 'w') as f:
         random_samples1 = [[random.randint(Control_X_low+2, Control_X_high-3),] for _ in range(batch_size)]
         random_samples2 = [[random.randint(Control_Y_low+2, Control_Y_high-3) ] for _ in range(batch_size)]
         random_samples3 = [[random.randint(Control_Z_low, Control_Z_high) ] for _ in range(batch_size)]
-        random_samples4 = [[random.uniform(Control_MOMY_low,Control_MOMY_high)] for _ in range(batch_size)]
+        random_samples4 = [[random.uniform(Control_Var_low,Control_Var_high)] for _ in range(batch_size)]
 
         # リストを結合してサンプルを作成
         combined_samples = [sample1 + sample2 + sample3 + sample4 for sample1, sample2, sample3, sample4 in zip(random_samples1, random_samples2, random_samples3, random_samples4)]
@@ -520,11 +521,11 @@ with open(log_file, 'w') as f:
             init = init_file.replace('######', str(pe).zfill(6))
             subprocess.run(["cp", org, init])
 
-        TEST_prec_sum, CTRL_prec_sum, TEST_prec_matrix, CTRL_prec_matrix , TEST_CTRL_prec_matrix=sim(optimal_inputs[0],optimal_inputs[1],optimal_inputs[2],optimal_inputs[3])## please change
+        TEST_prec_sum, CTRL_prec_sum, TEST_prec_matrix, CTRL_prec_matrix , TEST_CTRL_prec_matrix=sim(optimal_inputs[0],optimal_inputs[1],optimal_inputs[2],0)## please change
 
         end=time.time()
         time_diff = end - start
-        print(f'実行時間:{time_diff}\n')
+        print(f'\n\n実行時間:{time_diff}\n')
 
         print(f"CTRL_prec_matrix={CTRL_prec_matrix}")
         print(f"TEST_prec_matrix={TEST_prec_matrix}")
@@ -540,7 +541,7 @@ with open(log_file, 'w') as f:
 
         f.write(f"{TEST_prec_sum=}")
         f.write(f"{CTRL_prec_sum=}")
-        f.write(f"%={TEST_prec_sum/CTRL_prec_sum*100}%")
+        f.write(f"%={TEST_prec_sum/CTRL_prec_sum*100}%\n\n")
         plot_PREC(trial_i, TEST_CTRL_prec_matrix)
 
 
